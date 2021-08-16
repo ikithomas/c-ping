e05d6e374d925d8c6c308afee9625b74156962a8
Thomas Chui <thomas@ikithomas.com>
2021-08-17 02:29:37 +0800

    ex3 exaampel

--- ex3.c
+++ ex3.c
@@ -29,6 +29,12 @@ static bool is_adult(Person person) {
   return person.age >= 18;
 }
 
+static void print_personal_particular(Person p) {
+  printf("I am %d years old.\n", p.age);
+  printf("I am %d cm tall.\n", p.height);
+  printf("If you ask me if I am an adult, I say %s.\n", is_adult(p) ? "yes": "no");
+}
+
 int main() {
   Person thomas = (Person) {
     .age = 28,
@@ -37,9 +43,16 @@ int main() {
     .mother_tongue = Cantonese,
   };
 
-  printf("I am %d years old.\n", thomas.age);
-  printf("I am %d cm tall.\n", thomas.height);
-  printf("If you ask me if I am an adult, I say %s.\n", is_adult(thomas) ? "yes": "no");
+  print_personal_particular(thomas);
+
+  Person miranda = (Person) {
+    .age = 30,
+    .height = 160,
+    .origin = JP,
+    .mother_tongue = Cantonese,
+  };
+
+  print_personal_particular(miranda);
 
   return 0;
 }
