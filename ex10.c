ca62f81d3c72362045b2b79eac1f8ac0ce310b2f
Thomas Chui <thomas@ikithomas.com>
2021-08-19 22:34:49 +0800

    fix ex10

--- ex10.c
+++ ex10.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-  int i = 0;
-
-  for (i = 0; i < argc; i++) {
+  for (int i = 0; i < argc; i++) {
     printf("arg %d: %s\n", i, argv[i]);
   }
 
@@ -11,7 +9,7 @@ int main(int argc, char *argv[]){
 
   int num_states = 4;
 
-  for (i = 0; i < num_states; i++) {
+  for (int i = 0; i < num_states; i++) {
     printf("state %d: %s\n", i, states[i]);
   }
 
