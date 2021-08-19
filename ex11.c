4285029323ad03efb33dc1a8616b61901cab7cdf
Thomas Chui <thomas@ikithomas.com>
2021-08-19 22:50:28 +0800

    ex11

---
+++ ex11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int c, char *v[]){
+  int numbers[4] = { 0 };
+  char name[4] = { 'a' };
+
+  printf("Numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+  printf("Name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
+  printf("Name: %s\n", name);
+
+  numbers[0] = 1;
+  numbers[1] = 2;
+  numbers[2] = 3;
+  numbers[3] = 4;
+
+  name[0] = 'Z';
+  name[1] = 'e';
+  name[2] = 'd';
+  name[3] = '\0';
+
+  printf("Numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+  printf("Name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
+  printf("Name: %s\n", name);
+
+  return 0;
+}
